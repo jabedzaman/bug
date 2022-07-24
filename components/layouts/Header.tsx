06df4fefa0db0bb1d837963a0bf5393ee2fafe44
xense <99767817+thexense@users.noreply.github.com>
2022-07-24 17:29:52 +0530

    add: header & footer

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <div className='flex justify-between'>
+        <div>
+            img
+        </div>
+        <div>
+            <Link href='/'>Home</Link>
+            <Link href='/'>About</Link>
+            <Link href='/'>Projects</Link>
+            <Link href='/'>Blogs</Link>
+            <Link href='/'>Contact</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/layouts/Footer'
+import Header from '../components/layouts/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>xense | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header/>
+
       <div>
         this is dope
       </div>
+
+      <Footer/>
 
     </div>
   )
