d58bb928e2c24dad379649caea8836fe8764fa68
xense <99767817+thexense@users.noreply.github.com>
2022-07-24 21:14:43 +0530

    implement footer

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,8 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <hr />
+      <div className='flex flex-col justify-center items-center my-10'>
+        <div className='space-x-2'>
+          <span>
+           ©️ Copyright by  
+           </span>
+           <span>
+            <Link href='/'>xense</Link>
+           </span>
+        </div>
+        <div>
+          {/* socials */}
+        </div>
+      </div>
+    </div>
   )
 }
 
