a88ffc0572c514ed8e2533dd8b5eaa6db7957748
xense <99767817+thexense@users.noreply.github.com>
2022-07-24 21:30:26 +0530

    bug fix: large screen

--- components/containers/Hero.tsx
+++ components/containers/Hero.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 
 function Hero() {
   return (
-    <div className='flex min-h-screen lg:flex-row flex-col items-center justify-center lg:justify-between lg:mx-20 space-y-6 space-x-3'>
+    <div className='flex min-h-screen lg:flex-row flex-col items-center justify-center  lg:mx-20 space-y-6 space-x-3'>
         <div>
             <img src="/assets/images/xense.jpg" alt="xense"  width='200' className='rounded-full space-y-2 border-spacing-4 border-gray-200 border-4'/>
         </div>
         <div className='flex lg:flex-col flex-col space-y-3 justify-center items-center '>
           <div className='space-x-2 font-semibold text-xl lg:text-4xl'>
-          <span className=''>
+          <span className='lg:ml-4'>
             Hey! I am
           </span>
           <span className='text-blue-500'>
