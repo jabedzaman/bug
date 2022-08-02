8ee0381f5416585c3fa58d96b755c02f826c1edb
xense <99767817+thexense@users.noreply.github.com>
2022-08-02 17:26:09 +0530

    fix: padding

--- components/containers/Skills.tsx
+++ components/containers/Skills.tsx
@@ -1,92 +1,153 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 function Skills() {
   return (
-    <div className='flex flex-col space-y-10 items-center'>
-        <div className='font-semibold text-xl lg:text-4xl'>My Skills:</div>
-        <div>
-            <ul className='flex flex-row'>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+    <div className="flex flex-col space-y-10 items-center">
+      <div className="font-semibold text-xl lg:text-4xl">My Skills:</div>
+      <div>
+        <ul className="flex flex-row">
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-html-5.svg" alt="html5" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-html-5.svg"
+                alt="html5"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-css3.svg" alt="css3" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-css3.svg"
+                alt="css3"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-javascript.svg" alt="javascript" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-javascript.svg"
+                alt="javascript"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-typescript.svg" alt="typescript" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-            </ul>
-            <ul className='flex flex-row'>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-typescript.svg"
+                alt="typescript"
+                className=""
+              />
+            </div>
+          </li>
+        </ul>
+        <ul className="flex flex-row">
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-react.svg" alt="react" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-react.svg"
+                alt="react"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-tailwindcss.svg" alt="css3" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-tailwindcss.svg"
+                alt="css3"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-google-firebase-console.svg" alt="firebase" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-google-firebase-console.svg"
+                alt="f
+                irebase"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-visual-studio-code-2019.svg" alt="vscode" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-            </ul>
-            <ul className='flex flex-row'>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-visual-studio-code-2019.svg"
+                alt="vscode"
+                className=""
+              />
+            </div>
+          </li>
+        </ul>
+        <ul className="flex flex-row">
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-git.svg" alt="git" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-git.svg"
+                alt="git"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-github.svg" alt="github" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-github.svg"
+                alt="github"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-nodejs.svg" alt="node" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-              <li className='m-2 cursor-pointer '>
-                <Image 
+                src="/assets/images/icons8-nodejs.svg"
+                alt="node"
+                className=""
+              />
+            </div>
+          </li>
+          <li className="m-2 cursor-pointer ">
+            <div className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5'>
+              <Image
                 width={50}
                 height={50}
-                src="/assets/images/icons8-linux-48.png" alt="linux" className='transition-transform duration-150 ease-out hover:scale-110 mx-1 my-1 lg:mx-5 lg:my-5' />
-              </li>
-            </ul>
-        </div>
+                src="/assets/images/icons8-linux-48.png"
+                alt="linux"
+                className=""
+              />
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
