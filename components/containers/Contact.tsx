30a2acd15463e0b5e6b862cee4b47b95fdf0681c
xense <jabedzaman2004@gmail.com>
2022-09-08 17:37:31 +0530

    add: linkedin

--- components/containers/Contact.tsx
+++ components/containers/Contact.tsx
@@ -8,14 +8,23 @@ function Contact() {
       <div className="flex items-center justify-center my-10 lg:my-20 flex-col space-y-10">
         <span className="font-semibold text-xl lg:text-4xl">Get in touch:</span>
         <span className="flex flex-row">
-          <Link href="https://instagram.com/xenseeee" target="_blank">
+          {/* <Link href="https://instagram.com/xenseeee" target="_blank">
             <Image
               src="/assets/images/icons8-instagram.svg"
               width={50}
               height={50}
               alt="instagram"
               className="cursor-pointer mx-1 lg:mx-2 transition-transform duration-150 ease-out hover:scale-125 "
-            />
+            />            
+          </Link> */}
+          <Link href="https://linkedin.com/xenseee" target="_blank">
+            <Image
+              src="/assets/images/icons8-linkedin-logo-48.png"
+              width={45}
+              height={45}
+              alt="linkedin"
+              className="cursor-pointer mx-1 lg:mx-2 transition-transform duration-150 ease-out hover:scale-125 "
+            />            
           </Link>
           <Link href="https://twitter.com/xenseee" target="_blank">
             <Image
