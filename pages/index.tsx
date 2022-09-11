81373ac710ec5f5ceb5353cf1682eb8010d0deb9
xense <jabedzaman2004@gmail.com>
2022-09-11 15:30:43 +0530

    fix: tittle

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className="px-4 sm:px-8 md:px-24 py-4 lg:px-48 xl:px-72 text-white justify-between">
       <Head>
-        <title>xense | Home</title>
+        <title>xense | Home </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
