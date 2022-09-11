cd6812e38e98f63aa240a12e966bd0b26f1645a5
xense <jabedzaman2004@gmail.com>
2022-09-11 15:20:54 +0530

    Add: Dropdown feature

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,45 +1,93 @@
 import React from "react";
+import { Disclosure } from "@headlessui/react";
 import Link from "next/link";
-import {
-  HomeIcon,
-  InformationCircleIcon,
-  RssIcon,
-  MailOpenIcon,
-  BriefcaseIcon,
-} from "@heroicons/react/outline";
 
-function Navbar() {
+export default function Navbar() {
+  const leftmenu = [
+    {
+      label: "Home",
+      href: "/"
+    },
+    {
+      label: "About",
+      href: "/about"
+    },
+    {
+      label: "Services",
+      href: "/services"
+    },
+    {
+      label: "Blogs",
+      href: "/blogs"
+    },
+    {
+      label: "Contact",
+      href: "/contact"
+    },
+  ];
+
+  const mobilemenu = [...leftmenu,];
+
   return (
-    <div className="flex justify-center lg:py-8 py-4 text-white">
-      <div className="space-x-2 flex">
-        <div title="Home">
-          <Link href="/">
-            <HomeIcon className="lg:h-10 h-8 hover: duration-200" />
-          </Link>
-        </div>
-        <div title="About">
-          <Link href="/about">
-            <InformationCircleIcon className="lg:h-10 h-8 hover:rotate-12 duration-200" />
-          </Link>
-        </div>
-        <div title="Blogs">
-          <Link href="/blogs">
-            <RssIcon className="lg:h-10 h-8 hover:rotate-12 duration-200" />
-          </Link>
-        </div>
-        <div title="services">
-          <Link href="/services">
-            <BriefcaseIcon className="lg:h-10 h-8 hover:rotate-12 duration-200" />
-          </Link>
-        </div>
-        <div title="contact">
-          <Link href="/contact">
-            <MailOpenIcon className="lg:h-10 h-8 hover:rotate-12 duration-200" />
-          </Link>
-        </div>
-      </div>
-    </div>
+      <nav>
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <div className="flex flex-wrap justify-between md:gap-10 md:flex-nowrap">
+                <div className="flex-col items-center justify-start order-1 hidden w-full md:flex md:flex-row md:justify-end md:w-auto md:order-none md:flex-1">
+                  {leftmenu.map((item, index) => (
+                    <Link href={item.href} key={index}>
+                      <a className="px-5 py-2 text-sm font-semibold text-gray-600 dark:text-gray-400 hover:text-blue-500">
+                        {item.label}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+                <div className="flex justify-end w-full md:w-auto">
+                  <div>
+                    
+                  </div>
+                  <Disclosure.Button
+                    aria-label="Toggle Menu"
+                    className="px-2 py-1 ml-auto text-gray-500 rounded-md md:hidden focus:text-blue-500 focus:outline-none dark:text-gray-300 ">
+                    <svg
+                      className="w-6 h-6 fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24">
+                      {open && (
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                        />
+                      )}
+                      {!open && (
+                        <path
+                          fillRule="evenodd"
+                          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                        />
+                      )}
+                    </svg>
+                  </Disclosure.Button>
+                </div>
+              </div>
+              <Disclosure.Panel>
+                <div className="flex flex-col items-center justify-start order-2 w-full md:hidden">
+                  {mobilemenu.map((item, index) => (
+                    <Link href={item.href} key={index}>
+                      <a
+                        className="px-5 py-2 text-sm font-medium text-gray-600 dark:text-gray-400 hover:text-blue-500"
+                        target={item ? "_blank" : ""}
+                        rel={item ? "noopener" : ""}>
+                        {item.label}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </nav>
   );
 }
-
-export default Navbar;
