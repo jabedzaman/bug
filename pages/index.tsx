dd823067ecf256ffb8935efe64643f18a2e6397a
xense <jabedzaman2004@gmail.com>
2022-09-11 15:26:48 +0530

    implement: navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Hero from "../components/containers/Hero";
+import Navbar from "../components/layouts/Navbar";
 
 const Home: NextPage = () => {
   return (
-    <div className="px-4 sm:px-8 md:px-24 lg:px-48 xl:px-72 text-white justify-between">
+    <div className="px-4 sm:px-8 md:px-24 py-4 lg:px-48 xl:px-72 text-white justify-between">
       <Head>
         <title>xense | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar/>
       <Hero />
     </div>
   );
