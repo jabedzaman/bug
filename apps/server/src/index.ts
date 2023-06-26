1adb3af1c2d5c3bda8d589c232211c3d6c2a1ce2
bravo68web <jbandyopadhayaya@gmail.com>
2023-06-27 01:11:46 +0530

    imported @spotidl/core as monorepo module

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,8 @@
 import http from "http";
+import Spotify from "@spotidl/core/dist";
+
+// Initialize Spotify API with Auth Creds
+// const spotify = new Spotify({})
 
 const server = http.createServer((req, res) => {
   res.end("Hello World!");
