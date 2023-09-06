898acfb62b9fb8dfc30ba8365c206a26bb15990e
jabedzaman <hi@jabed.dev>
2023-09-06 13:57:53 +0530

    Remove element from double linked list

--- Linked Lists/doubleLinkedList.cpp
+++ Linked Lists/doubleLinkedList.cpp
@@ -85,6 +85,60 @@ public:
       }
     }
 
+    void remove(int index){
+      if(index == 0){
+        Node<T>* temp = head;
+        head = head->next;
+        head->prev = nullptr;
+        delete temp;
+      }
+      else if(index == size - 1){
+        Node<T>* temp = tail;
+        tail = tail->prev;
+        tail->next = nullptr;
+        delete temp;
+      }
+      else{
+        Node<T>* temp = head;
+        for(int i = 0; i < index - 1; i++){
+          temp = temp->next;
+        }
+        Node<T>* toDelete = temp->next;
+        temp->next = temp->next->next;
+        temp->next->prev = temp;
+        delete toDelete;
+      }
+      size--;
+    }
+
+    bool search(T value){
+      Node<T>* temp = head;
+      while(temp != nullptr){
+        if(temp->data == value){
+          return true;
+        }
+        temp = temp->next;
+      }
+      return false;
+    }
+
+    int getSize(){
+      return size;
+    }
+
+    int getDistance(T value){
+      Node<T>* temp = head;
+      int distance = 0;
+      while(temp != nullptr){
+        if(temp->data == value){
+          return distance;
+        }
+        distance++;
+        temp = temp->next;
+      }
+      return -1;
+    }
+
     ~DoubleLinkedList<T>(){
       Node<T>* temp = head;
       while(temp != nullptr){
@@ -113,5 +167,7 @@ int main()
   std::cin >> index >> value;
   list.insert(index, value);
   list.insertAtHead(10);
+  list.remove(2);
+  list.search(10) ? std::cout << "Found" << std::endl : std::cout << "Not Found" << std::endl;  
   list.display();
 }
