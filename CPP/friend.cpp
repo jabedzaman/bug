eb111e5f55b7aa2cf8ecd706f793b8299178b297
jabedzaman <hi@jabed.dev>
2023-09-06 13:47:17 +0530

    Add friend and pointer examples, insertAtHead and insert methods to doubleLinkedList.cpp

---
+++ CPP/friend.cpp
@@ -0,0 +1,5 @@
+/*
+ * friend function
+ * 1. friend function is not in the scope of the class to which it has been declared as a friend.
+ *
+ */
--- Linked Lists/doubleLinkedList.cpp
+++ Linked Lists/doubleLinkedList.cpp
@@ -50,6 +50,41 @@ public:
       std::cout << std::endl;
     }
 
+    void insertAtHead(T data){
+      Node<T>* newNode = new Node<T>(data);
+      if(head == nullptr){
+        head = newNode;
+        tail = newNode;
+      }
+      else{
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+      } 
+      size++;
+    }
+
+    void insert(int index, T value){
+      Node<T>* newNode = new Node<T>(value);
+      if(index == 0){
+        insertAtHead(value);
+      }
+      else if(index == size){
+        push_back(value);
+      }
+      else{
+        Node<T>* temp = head;
+        for(int i = 0; i < index - 1; i++){
+          temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next->prev = newNode;
+        temp->next = newNode;
+        newNode->prev = temp;
+        size++;
+      }
+    }
+
     ~DoubleLinkedList<T>(){
       Node<T>* temp = head;
       while(temp != nullptr){
@@ -72,5 +107,11 @@ int main()
     std::cin >> x;
     list.push_back(x);
   }
+  list.display();
+  std::cout << "Enter the index and value to insert: ";
+  int index, value;
+  std::cin >> index >> value;
+  list.insert(index, value);
+  list.insertAtHead(10);
   list.display();
 }
